845fb532ee2f55c3c87555ece8ea94c7e5a3d35f
Josh Miller <jxm7374@rit.edu>
2016-12-02 22:52:24 -0500

    More changes

--- src/main/java/com/example/josh/geosafe/MainActivity.java
+++ src/main/java/com/example/josh/geosafe/MainActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.pathsense.android.sdk.location.PathsenseLocationProviderApi;
+
 public class MainActivity extends AppCompatActivity {
+    public static final int RANGE = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
     public void registerGeofences(View view){
         //TODO: add geofences
+        PathsenseLocationProviderApi providerApi = PathsenseLocationProviderApi.getInstance(view.getContext());
+        Geofence[] geofences = getGeofences();
+        for(Geofence geofence: geofences){
+            if(geofence.getType().equals("caution")){
+//                providerApi.addGeofence(geofence.getId(),geofence.getLatitude(),geofence.getLongitude(),500,);
+            }
+        }
     }
 
     public void unregisterGeofences(View view){
-        //TODO: remove geofences
+        PathsenseLocationProviderApi providerApi = PathsenseLocationProviderApi.getInstance(view.getContext());
+        Geofence[] geofences = getGeofences();
+        providerApi.removeGeofences();
+        providerApi.destroy();
     }
 
     private Geofence[] getGeofences(){
